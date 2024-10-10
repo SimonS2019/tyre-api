c9aa5814c0c978a5162845e975039df23a803960
SimonS2019 <shentiexiong@gmail.com>
2024-10-11 11:11:06 +1300

    fix: The bin folder in the Express Generator is where the port is configured.

--- tyre-api/app.js
+++ tyre-api/app.js
@@ -32,9 +32,9 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 module.exports = app;
